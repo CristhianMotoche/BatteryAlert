16437f7b09c73ffaff85514d472e2766db001283
lomejordejr <lomejordejr@gmail.com>
2015-06-29 16:52:27 -0500

    Agregado alerta descarga

--- batteryProgram.sh
+++ batteryProgram.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+#lectura archivos
 fileCapacity="/sys/class/power_supply/BAT0/capacity"
 fileStatus="/sys/class/power_supply/BAT0/status"
 
-if [ -e $file ]; then
+if [ -e $fileCapacity ] && [ -e $fileStatus ]; then
+    
+    #tomar valores
     capacity=$(cat "$fileCapacity")
     status=$(cat "$fileStatus")
+
+    #alerta
     if [ $capacity -gt 85 ] && [ $status == "Charging" ] ; then
 	DISPLAY=:0.0 /usr/bin/notify-send "Desconectar computadora:" \
 		    "Actualmente $capacity\%"
-    else
-	DISPLAY=:0.0 /usr/bin/notify-send "Si funca"
+    elif [ $capacity -lt 15 ] && [ $status == "Discharging" ] ; then
+	DISPLAY=:0.0 /usr/bin/notify-send "Conectar computador: " \
+	       "Actualmente $capacity\%"	
     fi
-    
+else
+    DISPLAY=:0.0 /usr/bin/notify-send "No se encuentra los archivos"
 fi
