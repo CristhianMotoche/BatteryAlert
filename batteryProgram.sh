654504e47e9a174052a1913dc6b86a10e56334cb
lomejordejr <lomejordejr@gmail.com>
2015-08-16 17:08:39 -0500

    agregado alerta con imagenes

--- batteryProgram.sh
+++ batteryProgram.sh
@@ -3,6 +3,9 @@
 #lectura archivos
 fileCapacity="/sys/class/power_supply/BAT0/capacity"
 fileStatus="/sys/class/power_supply/BAT0/status"
+actualDir=`pwd`"/img/icon_battery-"
+imgHigh=$actualDir"high.svg"
+imgLow=$actualDir"low.svg"
 
 if [ -e $fileCapacity ] && [ -e $fileStatus ]; then
     
@@ -12,10 +15,10 @@ if [ -e $fileCapacity ] && [ -e $fileStatus ]; then
 
     #alerta
     if [ $capacity -gt 85 ] && [ $status == "Charging" ] ; then
-	DISPLAY=:0.0 /usr/bin/notify-send "Desconectar computadora:" \
+	DISPLAY=:0.0 /usr/bin/notify-send -i `echo $imgHigh` "Desconectar computadora:" \
 		    "Actualmente $capacity\%"
     elif [ $capacity -lt 15 ] && [ $status == "Discharging" ] ; then
-	DISPLAY=:0.0 /usr/bin/notify-send "Conectar computador: " \
+	DISPLAY=:0.0 /usr/bin/notify-send -i `echo $imgLow` "Conectar computador: " \
 	       "Actualmente $capacity\%"	
     fi
 else
